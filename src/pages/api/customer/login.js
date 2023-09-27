ff2929b5b9c85b55324c98f646d9c0bce5667a47
pinyoko573 <pinyoko573@gmail.com>
2023-09-28 01:11:52 +0800

    Add HTTPS cert to axios

--- src/pages/api/customer/login.js
+++ src/pages/api/customer/login.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
+const https = require('https');
 import axios from "axios";
+
+const httpsAgent = new https.Agent({
+  ca: fs.readFileSync(`${process.env.NEXT_PUBLIC_CA}`),        
+  cert: fs.readFileSync(`${process.env.NEXT_PUBLIC_CLIENT_CERT}`),
+  key: fs.readFileSync(`${process.env.NEXT_PUBLIC_CLIENT_KEY}`), 
+})
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -6,7 +14,8 @@ export default async function handler(req, res) {
       let server_req = await axios.post('https://ifs4205-23s1-1-2-i.comp.nus.edu.sg:8000/customer/login', req.body, {
         headers: {
           'Content-Type': 'application/json'
-        }
+        },
+        httpsAgent: httpsAgent
       })
       res.status(200).json(server_req.data);
     } catch (err) {
