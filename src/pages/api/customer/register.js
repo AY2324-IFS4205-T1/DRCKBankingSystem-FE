49a78e804b6f54d1bc93bce94477c6805bbd201b
pinyoko573 <pinyoko573@gmail.com>
2023-09-28 00:01:21 +0800

    Test Route API for customer login and register

---
+++ src/pages/api/customer/register.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      let server_req = await axios.post('http://localhost:8000/customer/register', req.body, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      res.status(200).json(server_req.data);
+    } catch (err) {
+      res.status(err.response.status).json(err.response.data);
+    }
+
+  } else {
+    // Method not allowed
+    res.status(405);
+  }
+}
