e2155070a61dac15a8aa20f66109248f866629c5
pinyoko573 <pinyoko573@gmail.com>
2023-10-01 13:57:35 +0800

    Temporarily fix cache issue on accounts returning same data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: () => {
+    return [
+      {
+        source: '/api/:slug*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
