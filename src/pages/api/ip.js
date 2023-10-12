c21291d3b5a93a374bfc1ba28b7e43c8caec78ea
darrenangwx <darrenawx@gmail.com>
2023-10-12 19:59:50 +0800

    IP testing

--- src/pages/api/ip.js
+++ src/pages/api/ip.js
@@ -1,12 +1,15 @@
 import api_axiosConfig from "./api_axiosConfig";
+import requestIp from "request-ip";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
-      console.log('?');
-      let server_req = await api_axiosConfig.get("/ip", {
-      });
-      res.status(server_req.status).json(server_req.data);
+      console.log("?");
+      const detectedIp = requestIp.getClientIp(req);
+      console.log(detectedIp);
+
+      let server_req = await api_axiosConfig.get("/ip", {});
+      res.status(server_req.status).json({ clientIp: detectedIp });
     } catch (server_req_err) {
       res.status(server_req_err.response.status).json(server_req_err.response.data);
     }
