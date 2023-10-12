f4b5785269cf522b12404bd929acebedb84173ae
pinyoko573 <pinyoko573@gmail.com>
2023-10-12 12:47:19 +0800

    Adding ip.js for testing

---
+++ src/pages/api/ip.js
@@ -0,0 +1,16 @@
+import api_axiosConfig from "./api_axiosConfig";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      let server_req = await api_axiosConfig.get("/ip", {
+      });
+      res.status(server_req.status).json(server_req.data);
+    } catch (server_req_err) {
+      res.status(server_req_err.response.status).json(server_req_err.response.data);
+    }
+  } else {
+    // Method not allowed
+    res.status(405);
+  }
+}
