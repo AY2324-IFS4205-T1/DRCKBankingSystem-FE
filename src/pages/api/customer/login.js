620b8cc06a3007e05968e553aefd3b646f3e97a4
darrenangwx <darrenawx@gmail.com>
2023-10-14 19:04:22 +0800

    test get csrf token

--- src/pages/api/customer/login.js
+++ src/pages/api/customer/login.js
@@ -1,5 +1,19 @@
 import api_axiosConfig from "../api_axiosConfig";
 import requestIp from "request-ip";
+import { getCookie } from "cookies-next";
+// function parseCSRFCookie(name, server_req) {
+//   let cookieValue = null;
+//   const cookies = server_req.headers["set-cookie"][0].split(";");
+//   for (const element of cookies) {
+//     const cookie = element.trim();
+//     // Does this cookie string begin with the name we want?
+//     if (cookie.substring(0, name.length + 1) === name + "=") {
+//       cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//       break;
+//     }
+//   }
+//   return cookieValue;
+// }
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -7,11 +21,23 @@ export default async function handler(req, res) {
       let server_req = await api_axiosConfig.post("/customer/login", req.body, {
         headers: {
           "Content-Type": "application/json",
-          "Client-IP": requestIp.getClientIp(req)
+          "Client-IP": requestIp.getClientIp(req),
         },
       });
 
       server_req.data.type = "customer";
+      console.log("HEADERS***************************************");
+      console.log(server_req.headers);
+      console.log("COOKIE HEADER****************************************");
+      console.log(server_req.headers["set-cookie"]);
+      console.log("COOKIE VALUE USING GET COOKIE************************");
+      const csrftoken = getCookie("csrftoken");
+      console.log(csrftoken);
+      console.log("SessionID*********************************************");
+      const sessionid = getCookie("sessionid");
+      console.log(sessionid);
+      console.log("DATA******************************************");
+      console.log(server_req.data);
 
       res.status(server_req.status).json(server_req.data);
     } catch (server_req_err) {
