d6c8b1778712fe5ce856766f3abeda8c88019d57
darrenangwx <darrenawx@gmail.com>
2023-10-14 19:27:39 +0800

    update cookie header

--- src/pages/api/customer/login.js
+++ src/pages/api/customer/login.js
@@ -30,16 +30,32 @@ export default async function handler(req, res) {
       console.log(server_req.headers);
       console.log("COOKIE HEADER****************************************");
       console.log(server_req.headers["set-cookie"]);
-      console.log("COOKIE VALUE USING GET COOKIE************************");
-      const csrftoken = getCookie("csrftoken", { req, res });
-      console.log(csrftoken);
-      console.log("SessionID*********************************************");
-      const sessionid = getCookie("sessionid", { req, res });
-      console.log(sessionid);
-      console.log("GET ALL COOKIES***************************************");
-      const allcookies = getCookies({ req, res });
+      // console.log("COOKIE VALUE USING GET COOKIE************************");
+      // const csrftoken = getCookie("csrftoken", { req, res });
+      // console.log(csrftoken);
+      // console.log("SessionID*********************************************");
+      // const sessionid = getCookie("sessionid", { req, res });
+      // console.log(sessionid);
       console.log("DATA******************************************");
       console.log(server_req.data);
+
+      const cookieHeader = server_req.headers["set-cookie"];
+      if (cookieHeader) {
+        const cookiesArray = cookieHeader[0].split("; ");
+
+        // Find the "csrftoken" cookie
+        const csrfTokenCookie = cookiesArray.find((cookie) => cookie.startsWith("csrftoken="));
+
+        if (csrfTokenCookie) {
+          // Extract the csrf token value
+          const csrfToken = csrfTokenCookie.split("=")[1];
+          console.log("CSRF Token:", csrfToken);
+        } else {
+          console.log("No CSRF token found in the headers.");
+        }
+      } else {
+        console.log("No set-cookie header found in the response headers.");
+      }
 
       res.status(server_req.status).json(server_req.data);
     } catch (server_req_err) {
