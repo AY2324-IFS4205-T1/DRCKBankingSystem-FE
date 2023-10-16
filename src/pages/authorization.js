7f73835fdc2e4258a292052da93d4a5d42cbee7f
darrenangwx <darrenawx@gmail.com>
2023-10-17 02:23:30 +0800

    update auth page

--- src/pages/authorization.js
+++ src/pages/authorization.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { page_permissions } from "../page_permissions";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
+//if required role undefined?
 async function checkUserAuthentication(authToken, required_role) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/auth_check`, {
     method: "POST",
@@ -14,62 +16,77 @@ async function checkUserAuthentication(authToken, required_role) {
   return res;
 }
 
-export default async function Authorization() {
+export default function Authorization() {
+  const [data, setData] = useState(null);
   const router = useRouter();
-  const { originalPath } = router.query;
-  console.log("ORIGINAL PATH: " + originalPath);
-  const authToken = sessionStorage.getItem("token");
-  const userType = sessionStorage.getItem("userType");
 
-  const requested_path = originalPath.split("/", 3).join("/");
-  const required_role = page_permissions[requested_path];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const authToken = sessionStorage.getItem("token") || "";
+        const userType = sessionStorage.getItem("userType") || "";
+        const originalPath = router.asPath;
+        console.log("Original Path: " + originalPath);
 
-  // To redirect portal type when authentication failed
-  let res = await checkUserAuthentication(authToken, required_role);
-  let status = res.status;
+        const url = new URL(originalPath, window.location.origin);
+        const requested_path = url.searchParams.get("originalPath");
+        console.log("Requested Path: " + requested_path);
 
-  if (status == HttpStatusCode.Ok) {
-    router.push(originalPath);
-  } else if (status == HttpStatusCode.Unauthorized) {
-    // Invalid or no token
-    if (userType === "") {
-      router.push("/");
-    }
+        const required_role = page_permissions[requested_path];
+        console.log("Required Role: " + required_role);
 
-    router.push(`/${userType}/login`);
-  } else if (status == HttpStatusCode.Forbidden) {
-    // Not authorised or 2FA authenticated
-    let data = await res.json();
+        // To redirect portal type when authentication failed
+        let res = await checkUserAuthentication(authToken, required_role);
+        let status = res.status;
+        console.log("Status: " + status);
 
-    if (!data.authorised) {
-      // no authorisation page?
-      router.push(`/${userType}/dashboard`);
-    } else if (!data.authenticated) {
-      // If page is setup/verify 2FA, allow the user to perform 2FA first
-      if (originalPath == `/${userType}/setup` || originalPath == `/${userType}/verify`) {
-        router.push(originalPath);
+        // if (status === HttpStatusCode.Ok) {
+        //   router.push(originalPath);
+        // } else if (status === HttpStatusCode.Unauthorized) {
+        //   // Invalid or no token
+        //   if (userType === "") {
+        //     router.push("/");
+        //   } else {
+        //     router.push(`/${userType}/login`);
+        //   }
+        // } else if (status === HttpStatusCode.Forbidden) {
+        //   // Not authorized or 2FA authenticated
+        //   let data = await res.json();
+
+        //   if (!data.authorised) {
+        //     // no authorization page?
+        //     router.push(`/${userType}/dashboard`);
+        //   } else if (!data.authenticated) {
+        //     // If the page is setup/verify 2FA, allow the user to perform 2FA first
+        //     if (originalPath === `/${userType}/setup` || originalPath === `/${userType}/verify`) {
+        //       router.push(originalPath);
+        //     }
+
+        //     if (data.authenticated_message === "User does not have 2FA set up.") {
+        //       router.push(`/${userType}/setup`);
+        //     } else if (
+        //       data.authenticated_message === "The session has changed, 2FA needs to be verified again." ||
+        //       data.authenticated_message === "2FA has not been verified." ||
+        //       data.authenticated_message === "2FA timeout, 2FA needs to be verified again."
+        //     ) {
+        //       router.push(`/${userType}/verify`);
+        //     }
+        //   }
+        // } else {
+        //   // unknown error page?
+        //   console.log(`Error expected with unhandled status ${status}`);
+        // }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
       }
+    };
 
-      if (data.authenticated_message == "User does not have 2FA set up.") {
-        router.push(`/${userType}/setup`);
-      } else if (
-        data.authenticated_message == "The session has changed, 2FA needs to be verified again." ||
-        data.authenticated_message == "2FA has not been verified." ||
-        data.authenticated_message == "2FA timeout, 2FA needs to be verified again."
-      ) {
-        router.push(`/${userType}/verify`);
-      }
-    }
-  } else {
-    // unknown err page?
-    console.log(`Error expected with unhandled status ${status}`);
-  }
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <p>Checking authorization.....</p>
-      </div>
-    </>
+    <div>
+      <p>Checking authorization.....</p>
+    </div>
   );
 }
