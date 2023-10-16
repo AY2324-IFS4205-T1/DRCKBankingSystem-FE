c33b1389716f5f6aa9d5d9c1527c863bab92b723
darrenangwx <darrenawx@gmail.com>
2023-10-16 22:01:55 +0800

    fix merge conflict

--- src/pages/api/customer/login.js
+++ src/pages/api/customer/login.js
@@ -1,6 +1,5 @@
 import api_axiosConfig from "../api_axiosConfig";
 import requestIp from "request-ip";
-import { getCookie } from "cookies-next";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -10,33 +9,11 @@ export default async function handler(req, res) {
         withCredentials: true,
         headers: {
           "Content-Type": "application/json",
-          "Client-IP": requestIp.getClientIp(req),
-          "X-CSRFToken": getCookie("csrftoken", { req, res }),
-          Referer: "https://ifs4205-23s1-1-1-i.comp.nus.edu.sg",
+          "Client-IP": requestIp.getClientIp(req)
         },
       });
 
       server_req.data.type = "customer";
-
-      const cookieHeader = server_req.headers["set-cookie"];
-      if (cookieHeader) {
-        const cookiesArray = cookieHeader[0].split("; ");
-
-        // Find the "csrftoken" cookie
-        const csrfTokenCookie = cookiesArray.find((cookie) => cookie.startsWith("csrftoken="));
-
-        if (csrfTokenCookie) {
-          // Extract the csrf token value
-          const csrfToken = csrfTokenCookie.split("=")[1];
-          // Append to data
-          server_req.data.csrftoken = csrfToken;
-          console.log("CSRF Token:", csrfToken);
-        } else {
-          console.log("No CSRF token found in the headers.");
-        }
-      } else {
-        console.log("No set-cookie header found in the response headers.");
-      }
 
       res.status(server_req.status).json(server_req.data);
     } catch (server_req_err) {
